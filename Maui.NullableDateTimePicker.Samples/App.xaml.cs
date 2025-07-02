8477a09b1bbc4ae495e6bad62a7016cfd816e863
Sebahattin Arslan <sarslan2016@gmail.com>
2025-07-02 21:19:03 +0200

    Removed CurrentUICulture and CurrentCulture assignments

--- Maui.NullableDateTimePicker.Samples/App.xaml.cs
+++ Maui.NullableDateTimePicker.Samples/App.xaml.cs
@@ -8,9 +8,6 @@ namespace Maui.NullableDateTimePicker.Samples
         {
             InitializeComponent();
 
-            System.Threading.Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("tr-TR");
-            System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("tr-TR");
-
             // Register a handler for unhandled XAML exceptions
             AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
         }
@@ -18,7 +15,6 @@ namespace Maui.NullableDateTimePicker.Samples
         protected override Window CreateWindow(IActivationState activationState)
         {
             var window = new Window(new AppShell());
-            //return new Window(new NavigationPage(new MainPage()));
             return window;
         }
 
